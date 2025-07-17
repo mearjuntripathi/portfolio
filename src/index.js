883bffe8bec6731640080d4eb964c80a79bf4919
mearjuntripathi <mearjuntripathi@gmail.com>
2025-07-17 22:36:54 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
 
